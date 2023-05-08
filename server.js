226b0eabc406a38507443da458f8fa2286fb5aa9
Jenna Perez <jmsp43@gmail.com>
2023-05-08 09:52:13 -0400

    tip calculator application created

--- server.js
+++ server.js
@@ -13,6 +13,10 @@ app.get('/greeting/:name', (req, res) => {
     res.send(`Hey, great to see you ${req.params.name}`)
 })
 
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    res.send(`Tip is $${(req.params.total * req.params.tipPercentage)/100}`)
+})
+
 //express listen here
 app.listen(port, () => {
     console.log(`Listening at port ${port}`)
